beed54bd1b0b1cb3ffb62323684383defbf0c60b
Tomas Sieger <tsieger@users.noreply.github.com>
2016-12-16 11:16:31 +0100

    computation of lighter colors improved

--- Gui_functions/colorSpikeMarkers.m
+++ Gui_functions/colorSpikeMarkers.m
@@ -14,7 +14,7 @@ function colorSpikeMarkers(spikeMarkers, spikesClass, colors)
                 clr=colors(1+mod(i-1,length(colors)),:);
                 % make the color a little bit lighter to ease
                 % its visual perception on the white background
-                clr=min([1 1 1],clr+[.2 .2 .2]);
+                clr=clr+(1-clr).*.25;
             else
                 % cluster 0 plotted in black
                 clr=[0 0 0];
